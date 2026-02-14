33135dfddc571a9949b2331e2a6d7ae5fee7576d
nahured <nahured.elzorro+guitea@gmail.com>
2026-02-13 23:22:09 -0300

    Add Get Tile tool UI, widget, and icon
    Introduce a new Get Tile tool and integrate it into the UI.
    
    - Added form definition for the tool: src/gui/formBuilder/widgets/get_tile_tool.fbp (wxFormBuilder panel with controls for selecting points, zoom, spin controls, and action buttons).
    - Added Python implementation for the widget: src/gui/modules/widgets/get_tile_tool.py.
    - Added icon resource used by the UI: src/gui/resources/icons/drop_file_icon.svg.
    - Updated container form and module to integrate the new tool: src/gui/formBuilder/windows_entity_container.fbp and src/gui/modules/windows_entity_container.py.
    
    These changes add the UI and backing code for generating tiles and selecting coordinate points, and wire the tool into the existing windows/entity container.

---
+++ src/gui/modules/widgets/get_tile_tool.py
@@ -0,0 +1,198 @@
+# -*- coding: utf-8 -*-
+
+###########################################################################
+## Python code generated with wxFormBuilder (version 4.2.1-0-g80c4cb6)
+## http://www.wxformbuilder.org/
+##
+## PLEASE DO *NOT* EDIT THIS FILE!
+###########################################################################
+
+import wx
+import wx.xrc
+
+import gettext
+_ = gettext.gettext
+
+###########################################################################
+## Class GetTileTool
+###########################################################################
+
+class GetTileTool ( wx.Panel ):
+
+    def __init__( self, parent, id = wx.ID_ANY, pos = wx.DefaultPosition, size = wx.Size( 500,400 ), style = wx.TAB_TRAVERSAL, name = wx.EmptyString ):
+        wx.Panel.__init__ ( self, parent, id = id, pos = pos, size = size, style = style, name = name )
+
+        bSizer4 = wx.BoxSizer( wx.VERTICAL )
+
+        self.Label_title = wx.StaticText( self, wx.ID_ANY, _(u"Herramienta para Generar Tiles"), wx.DefaultPosition, wx.DefaultSize, wx.ALIGN_CENTER_HORIZONTAL )
+        self.Label_title.Wrap( -1 )
+
+        self.Label_title.SetFont( wx.Font( wx.NORMAL_FONT.GetPointSize(), wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, wx.EmptyString ) )
+
+        bSizer4.Add( self.Label_title, 0, wx.EXPAND, 5 )
+
+        m_listBox2Choices = []
+        self.m_listBox2 = wx.ListBox( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, m_listBox2Choices, wx.LB_HSCROLL|wx.LB_MULTIPLE|wx.LB_SINGLE )
+        bSizer4.Add( self.m_listBox2, 0, wx.ALL|wx.EXPAND, 5 )
+
+        self.m_staticline1 = wx.StaticLine( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.LI_HORIZONTAL )
+        bSizer4.Add( self.m_staticline1, 0, wx.EXPAND |wx.ALL, 5 )
+
+        bSizer41 = wx.BoxSizer( wx.HORIZONTAL )
+
+        self.m_staticText1 = wx.StaticText( self, wx.ID_ANY, _(u"zoom L"), wx.DefaultPosition, wx.DefaultSize, 0 )
+        self.m_staticText1.Wrap( -1 )
+
+        bSizer41.Add( self.m_staticText1, 0, wx.ALL, 5 )
+
+        self.m_spinCtrl5 = wx.SpinCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, wx.SP_ARROW_KEYS, 0, 10, 0 )
+        bSizer41.Add( self.m_spinCtrl5, 1, wx.ALL, 5 )
+
+
+        bSizer4.Add( bSizer41, 0, wx.EXPAND, 5 )
+
+        bSizer5 = wx.BoxSizer( wx.HORIZONTAL )
+
+        self.label_point_1_x = wx.StaticText( self, wx.ID_ANY, _(u"X:"), wx.DefaultPosition, wx.DefaultSize, 0 )
+        self.label_point_1_x.Wrap( -1 )
+
+        bSizer5.Add( self.label_point_1_x, 0, wx.ALL, 5 )
+
+        self.spin_point_1_x = wx.SpinCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, wx.SP_ARROW_KEYS, 0, 10, 0 )
+        bSizer5.Add( self.spin_point_1_x, 1, wx.ALL, 5 )
+
+        self.label_point_1_y = wx.StaticText( self, wx.ID_ANY, _(u"Y:"), wx.DefaultPosition, wx.DefaultSize, 0 )
+        self.label_point_1_y.Wrap( -1 )
+
+        bSizer5.Add( self.label_point_1_y, 0, wx.ALL, 5 )
+
+        self.spin_point_1_y = wx.SpinCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, wx.SP_ARROW_KEYS, 0, 10, 0 )
+        bSizer5.Add( self.spin_point_1_y, 1, wx.ALL, 5 )
+
+        self.m_staticline2 = wx.StaticLine( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.LI_VERTICAL )
+        bSizer5.Add( self.m_staticline2, 0, wx.ALL|wx.EXPAND, 5 )
+
+        self.button_pick_point_1 = wx.Button( self, wx.ID_ANY, _(u"seleccionar punto 1"), wx.DefaultPosition, wx.DefaultSize, 0 )
+        bSizer5.Add( self.button_pick_point_1, 0, wx.ALL, 5 )
+
+
+        bSizer4.Add( bSizer5, 0, wx.EXPAND, 5 )
+
+        bSizer51 = wx.BoxSizer( wx.HORIZONTAL )
+
+        self.label_point_2_x = wx.StaticText( self, wx.ID_ANY, _(u"X:"), wx.DefaultPosition, wx.DefaultSize, 0 )
+        self.label_point_2_x.Wrap( -1 )
+
+        bSizer51.Add( self.label_point_2_x, 0, wx.ALL, 5 )
+
+        self.spin_point_2_x = wx.SpinCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, wx.SP_ARROW_KEYS, 0, 10, 0 )
+        bSizer51.Add( self.spin_point_2_x, 1, wx.ALL, 5 )
+
+        self.label_point_2_y = wx.StaticText( self, wx.ID_ANY, _(u"Y:"), wx.DefaultPosition, wx.DefaultSize, 0 )
+        self.label_point_2_y.Wrap( -1 )
+
+        bSizer51.Add( self.label_point_2_y, 0, wx.ALL, 5 )
+
+        self.spin_point_2_y = wx.SpinCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, wx.SP_ARROW_KEYS, 0, 10, 0 )
+        bSizer51.Add( self.spin_point_2_y, 1, wx.ALL, 5 )
+
+        self.m_staticline21 = wx.StaticLine( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.LI_VERTICAL )
+        bSizer51.Add( self.m_staticline21, 0, wx.ALL|wx.EXPAND, 5 )
+
+        self.button_pick_point_2 = wx.Button( self, wx.ID_ANY, _(u"seleccionar punto 2"), wx.DefaultPosition, wx.DefaultSize, 0 )
+        bSizer51.Add( self.button_pick_point_2, 0, wx.ALL, 5 )
+
+
+        bSizer4.Add( bSizer51, 0, wx.EXPAND, 5 )
+
+        self.m_staticline4 = wx.StaticLine( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.LI_HORIZONTAL )
+        bSizer4.Add( self.m_staticline4, 0, wx.EXPAND |wx.ALL, 5 )
+
+        self.button_generate_collage = wx.Button( self, wx.ID_ANY, _(u"crear Imagen"), wx.DefaultPosition, wx.DefaultSize, 0 )
+        bSizer4.Add( self.button_generate_collage, 0, wx.ALL|wx.EXPAND, 5 )
+
+        bSizer52 = wx.BoxSizer( wx.HORIZONTAL )
+
+        self.Drop_file = wx.StaticBitmap( self, wx.ID_ANY, wx.NullBitmap, wx.DefaultPosition, wx.DefaultSize, 0 )
+        bSizer52.Add( self.Drop_file, 1, wx.ALL|wx.EXPAND, 5 )
+
+        self.file_picker = wx.FilePickerCtrl( self, wx.ID_ANY, wx.EmptyString, _(u"seleccionar archivo ctile"), _(u"*.ctile*"), wx.DefaultPosition, wx.DefaultSize, wx.FLP_DEFAULT_STYLE )
+        bSizer52.Add( self.file_picker, 0, wx.ALL, 5 )
+
+
+        bSizer4.Add( bSizer52, 1, wx.EXPAND, 5 )
+
+
+        self.SetSizer( bSizer4 )
+        self.Layout()
+
+        # Connect Events
+        self.spin_point_1_x.Bind( wx.EVT_SPINCTRL, self.spin_point_1_x_OnSpinCtrl )
+        self.spin_point_1_x.Bind( wx.EVT_TEXT, self.spin_point_1_x_OnSpinCtrlText )
+        self.spin_point_1_x.Bind( wx.EVT_TEXT_ENTER, self.spin_point_2_x_OnTextEnter )
+        self.spin_point_1_y.Bind( wx.EVT_SPINCTRL, self.spin_point_1_y_OnSpinCtrl )
+        self.spin_point_1_y.Bind( wx.EVT_TEXT, self.spin_point_1_y_OnSpinCtrlText )
+        self.spin_point_1_y.Bind( wx.EVT_TEXT_ENTER, self.spin_point_1_y_OnTextEnter )
+        self.button_pick_point_1.Bind( wx.EVT_BUTTON, self.button_pick_point_1_OnButtonClick )
+        self.spin_point_2_x.Bind( wx.EVT_SPINCTRL, self.spin_point_2_x_OnSpinCtrl )
+        self.spin_point_2_x.Bind( wx.EVT_TEXT, self.spin_point_2_x_OnSpinCtrlText )
+        self.spin_point_2_x.Bind( wx.EVT_TEXT_ENTER, self.spin_point_2_x_OnTextEnter )
+        self.spin_point_2_y.Bind( wx.EVT_SPINCTRL, self.spin_point_2_y_OnSpinCtrl )
+        self.spin_point_2_y.Bind( wx.EVT_TEXT, self.spin_point_2_y_OnSpinCtrlText )
+        self.spin_point_2_y.Bind( wx.EVT_TEXT_ENTER, self.spin_point_2_y_OnTextEnter )
+        self.button_pick_point_2.Bind( wx.EVT_BUTTON, self.button_pick_point_2_OnButtonClick )
+        self.button_generate_collage.Bind( wx.EVT_BUTTON, self.button_generate_collage_OnButtonClick )
+        self.file_picker.Bind( wx.EVT_FILEPICKER_CHANGED, self.file_picker_OnFileChanged )
+
+    def __del__( self ):
+        pass
+
+
+    # Virtual event handlers, override them in your derived class
+    def spin_point_1_x_OnSpinCtrl( self, event ):
+        event.Skip()
+
+    def spin_point_1_x_OnSpinCtrlText( self, event ):
+        event.Skip()
+
+    def spin_point_2_x_OnTextEnter( self, event ):
+        event.Skip()
+
+    def spin_point_1_y_OnSpinCtrl( self, event ):
+        event.Skip()
+
+    def spin_point_1_y_OnSpinCtrlText( self, event ):
+        event.Skip()
+
+    def spin_point_1_y_OnTextEnter( self, event ):
+        event.Skip()
+
+    def button_pick_point_1_OnButtonClick( self, event ):
+        event.Skip()
+
+    def spin_point_2_x_OnSpinCtrl( self, event ):
+        event.Skip()
+
+    def spin_point_2_x_OnSpinCtrlText( self, event ):
+        event.Skip()
+
+
+    def spin_point_2_y_OnSpinCtrl( self, event ):
+        event.Skip()
+
+    def spin_point_2_y_OnSpinCtrlText( self, event ):
+        event.Skip()
+
+    def spin_point_2_y_OnTextEnter( self, event ):
+        event.Skip()
+
+    def button_pick_point_2_OnButtonClick( self, event ):
+        event.Skip()
+
+    def button_generate_collage_OnButtonClick( self, event ):
+        event.Skip()
+
+    def file_picker_OnFileChanged( self, event ):
+        event.Skip()
+
+
--- src/gui/modules/windows_entity_container.py
+++ src/gui/modules/windows_entity_container.py
@@ -9,6 +9,7 @@
 
 import wx
 import wx.xrc
+from src.gui.modules.widgets.get_tile_tool import GetTileTool
 
 import gettext
 _ = gettext.gettext
@@ -25,18 +26,80 @@ class WindowsEntityContainer ( wx.Panel ):
         bSizer3 = wx.BoxSizer( wx.VERTICAL )
 
         self.m_notebook1 = wx.Notebook( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, 0 )
-        self.m_panel1 = wx.Panel( self.m_notebook1, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
-        self.m_notebook1.AddPage( self.m_panel1, _(u"entidades"), True )
-        self.m_panel2 = wx.Panel( self.m_notebook1, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
-        self.m_notebook1.AddPage( self.m_panel2, _(u"capas"), False )
-        self.m_panel3 = wx.Panel( self.m_notebook1, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
+        self.tab_entity = wx.Panel( self.m_notebook1, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
+        bSizer4 = wx.BoxSizer( wx.VERTICAL )
+
+        self.tree_entity = wx.TreeCtrl( self.tab_entity, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TR_DEFAULT_STYLE )
+        bSizer4.Add( self.tree_entity, 1, wx.ALL|wx.EXPAND, 5 )
+
+        self.m_panel5 = wx.Panel( self.tab_entity, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
+        bSizer6 = wx.BoxSizer( wx.HORIZONTAL )
+
+        self.m_staticText1 = wx.StaticText( self.m_panel5, wx.ID_ANY, _(u"entidad"), wx.DefaultPosition, wx.DefaultSize, 0 )
+        self.m_staticText1.Wrap( -1 )
+
+        bSizer6.Add( self.m_staticText1, 0, wx.ALL, 5 )
+
+        self.entity_selected_name = wx.StaticText( self.m_panel5, wx.ID_ANY, _(u"temp_item"), wx.DefaultPosition, wx.DefaultSize, 0 )
+        self.entity_selected_name.Wrap( -1 )
+
+        bSizer6.Add( self.entity_selected_name, 0, wx.ALL, 5 )
+
+        self.button_entity_edit = wx.Button( self.m_panel5, wx.ID_ANY, _(u"editar"), wx.DefaultPosition, wx.DefaultSize, 0 )
+        bSizer6.Add( self.button_entity_edit, 0, wx.ALL, 5 )
+
+
+        self.m_panel5.SetSizer( bSizer6 )
+        self.m_panel5.Layout()
+        bSizer6.Fit( self.m_panel5 )
+        bSizer4.Add( self.m_panel5, 0, wx.EXPAND |wx.ALL, 5 )
+
+
+        self.tab_entity.SetSizer( bSizer4 )
+        self.tab_entity.Layout()
+        bSizer4.Fit( self.tab_entity )
+        self.m_notebook1.AddPage( self.tab_entity, _(u"entidades"), True )
+        self.tab_layer = wx.Panel( self.m_notebook1, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
+        bSizer7 = wx.BoxSizer( wx.VERTICAL )
+
+        self.m_scrolledWindow1 = wx.ScrolledWindow( self.tab_layer, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.HSCROLL|wx.VSCROLL )
+        self.m_scrolledWindow1.SetScrollRate( 5, 5 )
+        bSizer7.Add( self.m_scrolledWindow1, 1, wx.EXPAND |wx.ALL, 5 )
+
+        self.m_panel6 = wx.Panel( self.tab_layer, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
+        bSizer8 = wx.BoxSizer( wx.VERTICAL )
+
+
+        self.m_panel6.SetSizer( bSizer8 )
+        self.m_panel6.Layout()
+        bSizer8.Fit( self.m_panel6 )
+        bSizer7.Add( self.m_panel6, 1, wx.EXPAND |wx.ALL, 5 )
+
+
+        self.tab_layer.SetSizer( bSizer7 )
+        self.tab_layer.Layout()
+        bSizer7.Fit( self.tab_layer )
+        self.m_notebook1.AddPage( self.tab_layer, _(u"capas"), False )
+        self.tab_tool = wx.Panel( self.m_notebook1, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
         bSizer2 = wx.BoxSizer( wx.VERTICAL )
 
+        self.panel_get_tiles = wx.Panel( self.tab_tool, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
+        bSizer31 = wx.BoxSizer( wx.VERTICAL )
 
-        self.m_panel3.SetSizer( bSizer2 )
-        self.m_panel3.Layout()
-        bSizer2.Fit( self.m_panel3 )
-        self.m_notebook1.AddPage( self.m_panel3, _(u"Herramientas"), False )
+        self.Get_tile_tool = GetTileTool(self.panel_get_tiles)
+        bSizer31.Add( self.Get_tile_tool, 1, wx.ALL|wx.EXPAND, 5 )
+
+
+        self.panel_get_tiles.SetSizer( bSizer31 )
+        self.panel_get_tiles.Layout()
+        bSizer31.Fit( self.panel_get_tiles )
+        bSizer2.Add( self.panel_get_tiles, 1, wx.EXPAND |wx.ALL, 5 )
+
+
+        self.tab_tool.SetSizer( bSizer2 )
+        self.tab_tool.Layout()
+        bSizer2.Fit( self.tab_tool )
+        self.m_notebook1.AddPage( self.tab_tool, _(u"Herramientas"), False )
 
         bSizer3.Add( self.m_notebook1, 1, wx.EXPAND |wx.ALL, 5 )
 
@@ -47,6 +110,12 @@ class WindowsEntityContainer ( wx.Panel ):
         # Connect Events
         self.m_notebook1.Bind( wx.EVT_NOTEBOOK_PAGE_CHANGED, self.OnNoteBookPageChanged )
         self.m_notebook1.Bind( wx.EVT_NOTEBOOK_PAGE_CHANGING, self.OnNoteBookPageChangin )
+        self.tree_entity.Bind( wx.EVT_TREE_GET_INFO, self.tree_entity_OnTreeGetInfo )
+        self.tree_entity.Bind( wx.EVT_TREE_ITEM_ACTIVATED, self.tree_entity_OnItemActived )
+        self.tree_entity.Bind( wx.EVT_TREE_ITEM_GETTOOLTIP, self.tree_entity_OnTreeItemGetToolTip )
+        self.tree_entity.Bind( wx.EVT_TREE_ITEM_RIGHT_CLICK, self.tree_entity_OnTreeItemRigthClick )
+        self.tree_entity.Bind( wx.EVT_TREE_SET_INFO, self.tree_entity_onTreeSetInfo )
+        self.tree_entity.Bind( wx.EVT_TREE_STATE_IMAGE_CLICK, self.tree_entity_OnTreeStateImageClick )
 
     def __del__( self ):
         pass
@@ -57,6 +126,24 @@ class WindowsEntityContainer ( wx.Panel ):
         event.Skip()
 
     def OnNoteBookPageChangin( self, event ):
+        event.Skip()
+
+    def tree_entity_OnTreeGetInfo( self, event ):
+        event.Skip()
+
+    def tree_entity_OnItemActived( self, event ):
+        event.Skip()
+
+    def tree_entity_OnTreeItemGetToolTip( self, event ):
+        event.Skip()
+
+    def tree_entity_OnTreeItemRigthClick( self, event ):
+        event.Skip()
+
+    def tree_entity_onTreeSetInfo( self, event ):
+        event.Skip()
+
+    def tree_entity_OnTreeStateImageClick( self, event ):
         event.Skip()
 
 
