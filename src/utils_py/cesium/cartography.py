01389d637f459d9a0fe11175bf23e0e5e333dc51
nahured <nahured.elzorro+guitea@gmail.com>
2026-02-17 00:06:15 -0300

    add
    se agregaron funciones para transformar datos

--- src/utils_py/cesium/cartography.py
+++ src/utils_py/cesium/cartography.py
@@ -16,31 +16,54 @@ class TileCoord:
     def __init__(self,x:int=0,y:int=0):
         self.x:int = x
         self.y:int = y
+        self.invert_y:bool = True
     
     def get_tile_count(self,level) -> list[int,int]:
+        """
+        retorna la cantidad de tiles por nivel
+        """
         x = 2**(level+1)
         y = 2**(level)
         return x,y
     
     def get_tile_dvision(self,level) -> list[float,float]:
+        """
+        retorna la division de cuanto grados mide un tile
+        """
         l_x,l_y = self.get_tile_count(level)
         x = float(360/l_x)
         y = float(180/l_y)
         return x,y
 
-    def from_degrres(self,longitude,latitude,level):
+    def from_degrres(self,degrees:"Degrees",level) -> "TileCoord":
+        """
+        esta funcion te devuelve un TileCoord a partir de un Degrees
+        """
         l_x,l_y = self.get_tile_count(level)
+        long = degrees.longitude + 180
+        if self.invert_y:
+            lat = degrees.latitude -90
+        else:
+            lat = degrees.latitude +90
 
-        long = longitude + 180
-        lat = latitude -90
 
         x = np.floor(abs((long*l_x)/360))
         y = np.floor(abs((lat*l_y)/180))
 
         return TileCoord(x,y)
     
-    def to_degrees(self,level) -> "Degrees":
+    def to_degrees(self,h:float,v:float,level) -> "Degrees":
+        """
+        esta funcion te devuelve un Degrees a partir del tileCoord
+        """
         d_x,d_y = self.get_tile_dvision(level)
+        x = float((self.x+h)*d_x-180)
+        if self.invert_y:
+            y = float((self.y+v)*d_y+90)
+        else:
+            y = float((self.y+v)*d_y-90)
+        
+        return Degrees(x,y)
 
         
 
@@ -49,14 +72,31 @@ class Degrees:
         self.longitude:float = longitude
         self.latitude:float = latitude
     
-    def zero(self):
+    def zero(self) -> "Degrees":
         return Degrees(0.0,0.0)
+    
+    def from_tile_coord(self,tile:"TileCoord",h:float=0.0,v:float=0.0) -> "TileCoord":
+        """
+        trucazo
+        """
+        return tile.to_degrees(h,v)
 
 
 class Rect:
-    def __init__(self,point_1 = Degrees().zero(),point_2 = Degrees().zero()):
+    def __init__(self,point_1:Degrees = Degrees().zero(),point_2:Degrees = Degrees().zero()):
         self.point_1:Degrees = point_1
         self.point_2:Degrees = point_2
     
-    def from_tile(self,tile_1:TileCoord,tile_2:TileCoord):
-        pass
+    def from_tile(self,tile_1:"TileCoord",tile_2:"TileCoord") -> "Rect":
+        point_1 = tile_1.to_degrees()
+        point_2 = tile_1.to_degrees(1,1)
+        return Rect(point_1,point_2)
+    
+    def get_rect(self) -> dict:
+        rect = {
+        "Oeste" : self.point_1.latitude,
+        "Este" : self.point_2.latitude,
+        "Norte" : self.point_2.longitude,
+        "Sur" : self.point_1.longitude
+        }
+        return rect
